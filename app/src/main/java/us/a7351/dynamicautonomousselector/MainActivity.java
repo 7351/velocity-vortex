9d8a54e8af7d65740b376e9f69f4a8daf2a9af20
Leo Forney <forneyleo@gmail.com>
2016-10-09 22:56:02 -0500

    Run manual dialog when both apps installed also Decrease time from 8 seconds to 0.85 seconds for the splash screen

--- app/src/main/java/us/a7351/dynamicautonomousselector/MainActivity.java
+++ app/src/main/java/us/a7351/dynamicautonomousselector/MainActivity.java
@@ -2,7 +2,6 @@ package us.a7351.dynamicautonomousselector;
 
 import android.Manifest;
 import android.app.Activity;
-import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Environment;
@@ -23,7 +22,6 @@ import org.json.JSONObject;
 
 import java.io.BufferedOutputStream;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.Inet4Address;
@@ -37,25 +35,44 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    LinearLayout mainLayout;
-
-    List<Option> allOptions;
-
-    OptionTranslater translater;
-
-    Button pushButton;
-
-    HashMap<String, String> selectedItems;
-
     final static String TAG = "MainActivity";
-
+    // Storage Permissions
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
+    LinearLayout mainLayout;
+    List<Option> allOptions;
+    OptionTranslater translater;
+    Button pushButton;
+    HashMap<String, String> selectedItems;
     JsonHoster hoster;
 
+    // TODO: Add splash screen branding
     CoordinatorLayout coordinatorLayout;
-
     AlertDialog manualDialog;
 
-    // TODO: Add splash screen branding
+    /**
+     * Checks if the app has permission to write to device storage
+     * <p>
+     * If the app does not has permission then the user will be prompted to grant permissions
+     *
+     * @param activity
+     */
+    public static void verifyStoragePermissions(Activity activity) {
+        // Check if we have write permission
+        int permission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            // We don't have permission so prompt the user
+            ActivityCompat.requestPermissions(
+                    activity,
+                    PERMISSIONS_STORAGE,
+                    REQUEST_EXTERNAL_STORAGE
+            );
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -133,7 +150,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 if (appInstalledOrNot("com.qualcomm.ftcrobotcontroller")) {
                     postDataUsingPref();
                 }
-                if (!appInstalledOrNot("com.qualcomm.ftcrobotcontroller") && !appInstalledOrNot("com.qualcomm.ftcdriverstation")) {
+                if ((!appInstalledOrNot("com.qualcomm.ftcrobotcontroller") && !appInstalledOrNot("com.qualcomm.ftcdriverstation")) || (appInstalledOrNot("com.qualcomm.ftcrobotcontroller") && appInstalledOrNot("com.qualcomm.ftcdriverstation"))) {
                     Log.d(TAG, "Manual option");
                     AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
                     // set title
@@ -264,33 +281,5 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             System.gc();
         }
 
-    }
-
-    // Storage Permissions
-    private static final int REQUEST_EXTERNAL_STORAGE = 1;
-    private static String[] PERMISSIONS_STORAGE = {
-            Manifest.permission.READ_EXTERNAL_STORAGE,
-            Manifest.permission.WRITE_EXTERNAL_STORAGE
-    };
-
-    /**
-     * Checks if the app has permission to write to device storage
-     *
-     * If the app does not has permission then the user will be prompted to grant permissions
-     *
-     * @param activity
-     */
-    public static void verifyStoragePermissions(Activity activity) {
-        // Check if we have write permission
-        int permission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
-
-        if (permission != PackageManager.PERMISSION_GRANTED) {
-            // We don't have permission so prompt the user
-            ActivityCompat.requestPermissions(
-                    activity,
-                    PERMISSIONS_STORAGE,
-                    REQUEST_EXTERNAL_STORAGE
-            );
-        }
     }
 }
