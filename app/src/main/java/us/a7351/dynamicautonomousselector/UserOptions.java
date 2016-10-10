2a45e8c25fd0e95699ad701d1d0c2590cd47d9fe
Leo Forney <forneyleo@gmail.com>
2016-10-09 23:35:17 -0500

    Complete documentation for UserOptions

--- app/src/main/java/us/a7351/dynamicautonomousselector/UserOptions.java
+++ app/src/main/java/us/a7351/dynamicautonomousselector/UserOptions.java
@@ -12,14 +12,24 @@ class UserOptions {
              * new NumberOption("Title of the option", "hashMap key of the option),
              * You need the comma at the end unless it is the last option in the list
              * Example: A option where the drive team can enter in a value for the angle heading with a max value of 3 characters
-             * the hashmap key would be "gyroValue" so you can call selectorHashmap.get("gyroValue");
+             * the hashmap key would be "gyroValue" so you can call selectorHashmap.get("gyroValue"); in the op-mode code
              * You would write: new NumberOption(3, "Gyro value?", "gyroValue"), unless
              * @see NumberOption
             */
 
+            /**
+             * RadioOption:
+             * The way to have the drive team pick from a pre-given list of items is in the following format:
+             * new RadioOption(String[]{"Option1", "Option2", "Option3"}, "Title of option", "hashMap key of the option),
+             * You need the comma at the end unless it is the last in the list
+             * Example: An option where the drive team can choose what alliance they're on for the current match
+             * the hashmap key would be "alliance" so you can call selectorHashmap.get("alliance"); in the op-mode code
+             * You would write: new RadioOption(new String[]{"Red", "Blue"}, "Alliance?", "alliance");
+             * @see RadioOption
+            */
+
             new NumberOption("Prox minimum", "proxkey"),
             new RadioOption(new String[]{"Kentucky", "Hawaii", "Mountain", "Center vortex"}, "Target", "target"),
-            new RadioOption(new String[]{"Red", "Blue"}, "Alliance", "alliance"),
-            new NumberOption("Distance", "distancekey")
+            new RadioOption(new String[]{"Red", "Blue"}, "Alliance", "alliance")
     };
 }
