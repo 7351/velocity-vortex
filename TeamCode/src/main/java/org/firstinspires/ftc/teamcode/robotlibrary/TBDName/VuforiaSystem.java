57168845e7c2ef15d5c7e3ae95c1da19529894e0
Leo Forney <forneyleo@gmail.com>
2016-10-16 13:22:02 -0500

    Create autonomous programs on separate branches for driving to the cap ball

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotlibrary/TBDName/VuforiaSystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotlibrary/TBDName/VuforiaSystem.java
@@ -1,10 +1,10 @@
 package org.firstinspires.ftc.teamcode.robotlibrary.TBDName;
 
 import com.qualcomm.robotcore.util.RobotLog;
-import com.vuforia.Trackable;
 
 import org.firstinspires.ftc.robotcore.external.ClassFactory;
 import org.firstinspires.ftc.robotcore.external.matrices.OpenGLMatrix;
+import org.firstinspires.ftc.robotcore.external.matrices.VectorF;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
@@ -20,8 +20,8 @@ import java.util.List;
 public class VuforiaSystem {
 
     private final float MM_PER_INCH = 25.4f;
-    private final float ROBOT_WIDTH = 18*MM_PER_INCH;               // in mm
-    private final float FTC_FIELD_WIDTH = (12*12 - 2)*MM_PER_INCH;  // in mm
+    private final float ROBOT_WIDTH = 18 * MM_PER_INCH;               // in mm
+    private final float FTC_FIELD_WIDTH = (12 * 12 - 2) * MM_PER_INCH;  // in mm
     private final float TARGET_HEIGHT = 160.0f;                     // in mm
 
     public VuforiaLocalizer vuforia;
@@ -31,7 +31,7 @@ public class VuforiaSystem {
     private static final String VUFORIA_LICENSE_KEY = "AbXnxf//////AAAAGRuNC5J8ZEyftEBQmHGLn/JRAsckJezlsbt+FqzEIevPs5nHoqNr8RxWAOXkyTKIYfEkL17legkgm4sV7qv3qcJXlVQE1Xlo/UKbwVQBgzEfGZi9M3d3tgaJNLEeDe1VLXCVrGyrGSThbd364UF/+nsZMhnFGcnLavxaH8N0QWS5QiAgdbV71V4SLS2vWzML4leBiAxl8qqitSqHEmlez4xF5BoyADuT3lLanURW+g+guX7jFo8ONDzI+xjBsi5BCnI41USBfJdhRnh272sUgdpJFetdTQKIlvRifwHOzGz9oX1WpFSOid+NE76fLon5sHVRx4ztQrqBtSQN3J9CgaJo0DjkDyTMbJBTTE56n2Yi";
     private static final VuforiaLocalizer.CameraDirection DIRECTION = VuforiaLocalizer.CameraDirection.BACK;
     private static final String FIELD_DATABASE_NAME = "FTC_2016-17";
-    OpenGLMatrix phoneLocationMatrix = locationMatrix(90.0f, 0.0f, 0.0f, 0.0f, ROBOT_WIDTH/2.0f, 0.0f);
+    OpenGLMatrix phoneLocationMatrix = locationMatrix(90.0f, 0.0f, 0.0f, 0.0f, ROBOT_WIDTH / 2.0f, 0.0f);
     /* END OF PREFERENCES */
 
     /**
@@ -96,15 +96,16 @@ public class VuforiaSystem {
 
     public void loadFieldDatabase() {
         VuforiaTrackables fieldTrackables = vuforia.loadTrackablesFromAsset(FIELD_DATABASE_NAME);
-        addToTrackableList(specifyTrackable(fieldTrackables, 0, "wheels", locationMatrix(90.0f, 0.0f, 0.0f, 12.0f*MM_PER_INCH, FTC_FIELD_WIDTH/2.0f, TARGET_HEIGHT)));
-        addToTrackableList(specifyTrackable(fieldTrackables, 1, "tools", locationMatrix(90.0f, 0.0f, 90.0f, -FTC_FIELD_WIDTH/2.0f, 30.0f*MM_PER_INCH, TARGET_HEIGHT)));
-        addToTrackableList(specifyTrackable(fieldTrackables, 2, "legos", locationMatrix(90.0f, 0.0f, 0.0f, -30.0f*MM_PER_INCH, FTC_FIELD_WIDTH/2.0f, TARGET_HEIGHT)));
-        addToTrackableList(specifyTrackable(fieldTrackables, 3, "gears", locationMatrix(90.0f, 0.0f, 90.0f, -FTC_FIELD_WIDTH/2.0f, -12.0f*MM_PER_INCH, TARGET_HEIGHT)));
+        addToTrackableList(specifyTrackable(fieldTrackables, 0, "wheels", locationMatrix(90.0f, 0.0f, 0.0f, 12.0f * MM_PER_INCH, FTC_FIELD_WIDTH / 2.0f, TARGET_HEIGHT)));
+        addToTrackableList(specifyTrackable(fieldTrackables, 1, "tools", locationMatrix(90.0f, 0.0f, 90.0f, -FTC_FIELD_WIDTH / 2.0f, 30.0f * MM_PER_INCH, TARGET_HEIGHT)));
+        addToTrackableList(specifyTrackable(fieldTrackables, 2, "legos", locationMatrix(90.0f, 0.0f, 0.0f, -30.0f * MM_PER_INCH, FTC_FIELD_WIDTH / 2.0f, TARGET_HEIGHT)));
+        addToTrackableList(specifyTrackable(fieldTrackables, 3, "gears", locationMatrix(90.0f, 0.0f, 90.0f, -FTC_FIELD_WIDTH / 2.0f, -12.0f * MM_PER_INCH, TARGET_HEIGHT)));
         enableAllTrackables();
     }
 
     /**
      * This function adds a trackable to the allTrackables list
+     *
      * @param trackable The VuforiaTrackable object that you want to add
      */
     public void addToTrackableList(VuforiaTrackable trackable) {
@@ -123,6 +124,7 @@ public class VuforiaSystem {
 
     /**
      * This command adds all trackables in a VuforiaTrackables object to the allTrackables list.
+     *
      * @param trackables a VuforiaTrackables object that you want to add to the list.
      */
     public void addToTrackableList(VuforiaTrackables trackables) {
@@ -131,10 +133,11 @@ public class VuforiaSystem {
 
     /**
      * This function sets information on a trackable from a vuforiatrackables object.
+     *
      * @param TrackableDatabase the object that the trackable exists in.
-     * @param XMLIndex the number of the vuforiatrackable. Zerod indexed.
-     * @param Name the name you want to set the trackable to.
-     * @param Location an OpenGLMatrix to set the trackables position to.
+     * @param XMLIndex          the number of the vuforiatrackable. Zerod indexed.
+     * @param Name              the name you want to set the trackable to.
+     * @param Location          an OpenGLMatrix to set the trackables position to.
      * @return a vuforiatrackable object.
      */
     public VuforiaTrackable specifyTrackable(VuforiaTrackables TrackableDatabase, int XMLIndex, String Name, OpenGLMatrix Location) {
@@ -149,6 +152,32 @@ public class VuforiaSystem {
     // Same master method without OpenGLMatrix. This is used for non-static positional objects, like balls.
     public VuforiaTrackable specifyTrackable(VuforiaTrackables TrackableDatabase, int XMLIndex, String Name) {
         return specifyTrackable(TrackableDatabase, XMLIndex, Name, null);
+    }
+
+    public enum TargetDirection {
+        HORIZONTAL,
+        VERTICAL
+    }
+
+    /*
+    public double angleBetweenTargets(OpenGLMatrix PhoneLocationMatrix, OpenGLMatrix TargetLocationMatrix, TargetDirection Direction) {
+        VectorF phoneVector = PhoneLocationMatrix.getTranslation();
+        VectorF targetVector = TargetLocationMatrix.getTranslation();
+        float[] orderedPairs = {phoneVector.get(0), phoneVector.get(2),
+                targetVector.get(0), phoneVector.get(2)};
+
+
+    }
+    */
+
+    public double distanceBetweenTargets(OpenGLMatrix PhoneLocationMatrix, OpenGLMatrix TargetLocationMatrix) {
+        VectorF phoneVector = PhoneLocationMatrix.getTranslation();
+        VectorF targetVector = TargetLocationMatrix.getTranslation();
+        double[] orderedPairPhone = {phoneVector.get(0), phoneVector.get(2)};
+        double[] orderedPairTarget = {targetVector.get(0), phoneVector.get(2)};
+        double differenceX = orderedPairPhone[0] - orderedPairTarget[0];
+        double differenceZ = orderedPairPhone[1] - orderedPairTarget[1];
+        return Math.abs(Math.sqrt(Math.pow(differenceX, 2) + Math.pow(differenceZ, 2)));
     }
 
 
