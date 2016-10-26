05e096861afe7ecb4f69c70df3fe93f85b9ceb7b
Leo Forney <forneyleo@gmail.com>
2016-10-25 21:00:55 -0500

    Add Color sensor and stage new function for gyro turning

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ColorSensorTest.java
@@ -0,0 +1,42 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+
+import org.firstinspires.ftc.teamcode.robotlibrary.TBDName.ColorUtils;
+
+/**
+ * Created by Dynamic Signals on 10/25/2016.
+ */
+@Autonomous(name = "ColorTest", group = "Testing")
+public class ColorSensorTest extends OpMode {
+
+    ColorUtils colorUtils;
+
+    @Override
+    public void init() {
+
+        colorUtils = new ColorUtils(hardwareMap);
+
+    }
+
+    @Override
+    public void start() {
+
+        colorUtils.lineColorSensor.enableLed(true);
+
+    }
+
+    @Override
+    public void loop() {
+
+        telemetry.addData("Bottom", colorData(colorUtils.lineColorSensor));
+        telemetry.addData("Beacon", colorData(colorUtils.beaconColorSensor));
+
+    }
+
+    private String colorData(ColorSensor sensor) {
+        return String.valueOf("R: " + sensor.red() + " G: " + sensor.green() + " B: " + sensor.blue());
+    }
+}
