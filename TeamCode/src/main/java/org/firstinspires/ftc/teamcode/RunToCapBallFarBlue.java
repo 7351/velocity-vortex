1646c648ee73dbe2496964c4f4c348248b203509
Natalie Koenig <geekgirl5050@gmail.com>
2016-11-05 21:20:47 -0500

    The new code is the autonomous with 2 particles :)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RunToCapBallFarBlue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RunToCapBallFarBlue.java
@@ -31,6 +31,12 @@ public class RunToCapBallFarBlue extends OpMode {
 
     GyroSensor gyro;
 
+    DcMotor FlyWheelMotor;
+
+    DcMotor IntakeB;
+
+    DcMotor IntakeA;
+
 
 
     @Override
@@ -39,6 +45,9 @@ public class RunToCapBallFarBlue extends OpMode {
         driveTrain = new DriveTrain(hardwareMap);
         gyroUtils = new GyroUtils(hardwareMap, driveTrain, telemetry);
         colorUtils = new ColorUtils(hardwareMap);
+        FlyWheelMotor = hardwareMap.dcMotor.get("FlyWheelMotor");
+        IntakeB = hardwareMap.dcMotor.get("IntakeB");
+        IntakeA = hardwareMap.dcMotor.get("IntakeA");
 
 
         gyro = gyroUtils.gyro;
@@ -66,8 +75,7 @@ public class RunToCapBallFarBlue extends OpMode {
 
         if (stage == 1) { //drives forward 0.25 seconds
             if (time.time() <= 0.25) {
-                driveTrain.powerLeft(1);
-                driveTrain.powerRight(1);
+                driveTrain.driveStraight();
             } else {
                 driveTrain.stopRobot();
                 stage++;
@@ -83,7 +91,7 @@ public class RunToCapBallFarBlue extends OpMode {
         }
 
         if (stage == 3){
-            if(gyro.getHeading() < 30)
+            if(gyro.getHeading() < 32.5 || gyro.getHeading() > 350)
             {
                 driveTrain.powerLeft(.15);
                 driveTrain.powerRight(-.15);
@@ -100,15 +108,84 @@ public class RunToCapBallFarBlue extends OpMode {
             }
         }
 
-        if (stage == 5) {
-            if (time.time() < 2) {
+        if (stage == 5)
+        {
+            if (time.time() < 1.35)
+            {
+                driveTrain.driveStraight();
+            }
+            else
+            {
+                driveTrain.stopRobot();
+                stage++;
+                time.reset();
+                FlyWheelMotor.setPower(.60);
+            }
+        }
+
+        if(stage == 6)
+        {
+            if (time.time() > 2)
+            {
+                time.reset();
+                stage++;
+            }
+        }
+
+        if (stage == 7)
+        {
+            if(time.time() < 2)
+            {
+                IntakeB.setPower(1);
+            }
+            else
+            {
+                time.reset();
+                stage++;
+            }
+        }
+
+        if (stage == 8)
+        {
+            if (time.time() < .35)
+                IntakeA.setPower(1);
+            else
+            {
+                time.reset();
+                stage++;
+            }
+        }
+
+        if (stage == 9)
+        {
+            if(time.time() > 2)
+            {
+                IntakeB.setPower(0);
+                IntakeA.setPower(0);
+                FlyWheelMotor.setPower(0);
+                time.reset();
+                stage++;
+            }
+        }
+
+        if (stage == 10)
+        {
+            if (time.time() > .25)
+            {
+                time.reset();
+                stage++;
+            }
+        }
+
+
+        if (stage == 11) {
+            if (time.time() < 1) {
                 driveTrain.driveStraight();
             } else {
                 driveTrain.stopRobot();
                 stage++;
             }
         }
-
         telemetry.addData("Stage", String.valueOf(stage));
         telemetry.addData("Gyro", String.valueOf(gyro.getHeading()));
         telemetry.addData("Time", String.valueOf(time.time()));
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RunToCapBallFarRed.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RunToCapBallFarRed.java
@@ -1,13 +1,10 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.ftccommon.DbgLog;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.GyroSensor;
 import com.qualcomm.robotcore.util.ElapsedTime;
-import com.qualcomm.robotcore.util.Range;
 
 import org.firstinspires.ftc.teamcode.robotlibrary.TBDName.ColorUtils;
 import org.firstinspires.ftc.teamcode.robotlibrary.TBDName.DriveTrain;
@@ -31,6 +28,10 @@ public class RunToCapBallFarRed extends OpMode {
 
     GyroSensor gyro;
 
+    DcMotor FlyWheelMotor;
+
+    DcMotor IntakeB;
+
 
 
     @Override
@@ -39,11 +40,12 @@ public class RunToCapBallFarRed extends OpMode {
         driveTrain = new DriveTrain(hardwareMap);
         gyroUtils = new GyroUtils(hardwareMap, driveTrain, telemetry);
         colorUtils = new ColorUtils(hardwareMap);
+        FlyWheelMotor = hardwareMap.dcMotor.get("FlyWheelMotor");
+        IntakeB = hardwareMap.dcMotor.get("IntakeB");
 
 
         gyro = gyroUtils.gyro;
         gyro.calibrate();
-
 
     }
 
@@ -66,8 +68,7 @@ public class RunToCapBallFarRed extends OpMode {
 
         if (stage == 1) { //drives forward 0.25 seconds
             if (time.time() <= 0.25) {
-                driveTrain.powerLeft(1);
-                driveTrain.powerRight(1);
+                driveTrain.driveStraight();
             } else {
                 driveTrain.stopRobot();
                 stage++;
@@ -100,12 +101,58 @@ public class RunToCapBallFarRed extends OpMode {
             }
         }
 
-        if (stage == 5) {
-            if (colorUtils.lineColorSensor.red() == 0  || time.time() < 5) {
-                driveTrain.powerRight(.25);
-                driveTrain.powerLeft(.25);
+        if (stage == 5)
+        {
+            if (time.time() < 1.35)
+            {
+                driveTrain.driveStraight();
+            }
+            else
+            {
+                driveTrain.stopRobot();
+                stage++;
+                time.reset();
+                FlyWheelMotor.setPower(.60);
+            }
+        }
 
-                //driveTrain.driveStraight();
+        if(stage == 6)
+        {
+            if (time.time() > 2)
+            {
+                time.reset();
+                stage++;
+            }
+        }
+
+        if (stage == 7)
+        {
+            if(time.time() < 2)
+            {
+                IntakeB.setPower(1);
+            }
+            else
+            {
+                IntakeB.setPower(0);
+                FlyWheelMotor.setPower(0);
+                time.reset();
+                stage++;
+            }
+        }
+
+        if (stage ==8)
+        {
+            if (time.time() > .25)
+            {
+                time.reset();
+                stage++;
+            }
+        }
+
+
+        if (stage == 9) {
+            if (time.time() < 1) {
+                driveTrain.driveStraight();
             } else {
                 driveTrain.stopRobot();
                 stage++;
@@ -116,6 +163,7 @@ public class RunToCapBallFarRed extends OpMode {
         telemetry.addData("Stage", String.valueOf(stage));
         telemetry.addData("Gyro", String.valueOf(gyro.getHeading()));
         telemetry.addData("Time", String.valueOf(time.time()));
+        // telemetry.addData("Fly Wheel Power: ", String.valueOf())
 
     }
 }
