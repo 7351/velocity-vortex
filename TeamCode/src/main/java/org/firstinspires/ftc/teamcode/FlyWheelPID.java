c1dd3a35b6725a3e2cbd17ece94fe88d3510f27b
Leo Forney <forneyleo@gmail.com>
2017-01-02 15:35:21 -0600

    Kevin's expiremental teleop program with the cr servo and fly wheel PID now working (the current encoders are broken, use a different motor and it works)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FlyWheelPID.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FlyWheelPID.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.ftccommon.DbgLog;
 import com.qualcomm.robotcore.eventloop.opmode.*;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
 import org.firstinspires.ftc.teamcode.robotlibrary.TBDName.DriveTrain;
@@ -20,7 +21,10 @@ public class FlyWheelPID extends OpMode {
     private boolean DPadRight = false;
     private boolean DPadLeft = false;
     DriveTrain driveTrain;
+    int lastRPM;
+    int lastEncoder = 0;
     FlyWheel flyWheel;
+    ElapsedTime testingTime = new ElapsedTime();
 
 
     @Override
@@ -29,21 +33,14 @@ public class FlyWheelPID extends OpMode {
         driveTrain = new DriveTrain(hardwareMap);
         flyWheel = new FlyWheel(hardwareMap);
 
+        flyWheel.FlyWheelMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
         flyWheel.FlyWheelMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
     }
 
     @Override
-    public void loop() {
-
-                /*
-         * Fly wheel controls
-         * D-pad up - Increment the power by 0.05
-         * D-pad down - Decrease the power by 0.05
-         * D-pad right - Kill the motor
-         * D-pad left - Set the power to default no-matter the position
-         */
-
+    public void init_loop() {
         boolean DPadUpPressed = gamepad1.dpad_up;
         boolean DPadDownPressed = gamepad1.dpad_down;
         boolean DPadRightPressed = gamepad1.dpad_right;
@@ -51,7 +48,6 @@ public class FlyWheelPID extends OpMode {
 
         if (DPadDownPressed) {
             if (!DPadDown) {
-                flyWheel.currentlyRunning = true;
                 flyWheel.currentPower -= flyWheel.incrementValue;
                 DPadDown = true;
             }
@@ -61,7 +57,6 @@ public class FlyWheelPID extends OpMode {
 
         if (DPadUpPressed) {
             if (!DPadUp) {
-                flyWheel.currentlyRunning = true;
                 flyWheel.currentPower += flyWheel.incrementValue;
                 DPadUp = true;
             }
@@ -69,24 +64,84 @@ public class FlyWheelPID extends OpMode {
             DPadUp = false;
         }
 
-        if (DPadRightPressed) {
-            if (!DPadRight) {
-                flyWheel.currentlyRunning = false;
-                DPadRight = true;
+        telemetry.addData("FlyWheel", flyWheel.currentPower);
+    }
+
+    @Override
+    public void start() {
+        testingTime.reset();
+        flyWheel.currentlyRunning = true;
+    }
+
+    @Override
+    public void loop() {
+
+        if (testingTime.time() < 15) {
+
+            /*
+         * Fly wheel controls
+         * D-pad up - Increment the power by 0.05
+         * D-pad down - Decrease the power by 0.05
+         * D-pad right - Kill the motor
+         * D-pad left - Set the power to default no-matter the position
+         */
+
+            boolean DPadUpPressed = gamepad1.dpad_up;
+            boolean DPadDownPressed = gamepad1.dpad_down;
+            boolean DPadRightPressed = gamepad1.dpad_right;
+            boolean DPadLeftPressed = gamepad1.dpad_left;
+
+            if (DPadDownPressed) {
+                if (!DPadDown) {
+                    flyWheel.currentPower -= flyWheel.incrementValue;
+                    DPadDown = true;
+                }
+            } else {
+                DPadDown = false;
             }
-        } else {
-            DPadRight = false;
+
+            if (DPadUpPressed) {
+                if (!DPadUp) {
+                    flyWheel.currentPower += flyWheel.incrementValue;
+                    DPadUp = true;
+                }
+            } else {
+                DPadUp = false;
+            }
+
+            if (DPadRightPressed) {
+                if (!DPadRight) {
+                    if (!flyWheel.currentlyRunning) {
+                        flyWheel.currentlyRunning = true;
+                    } else {
+                        flyWheel.currentlyRunning = false;
+                    }
+                    DPadRight = true;
+                }
+            } else {
+                DPadRight = false;
+            }
+
+            if (DPadLeftPressed) {
+                if (!DPadLeft) {
+                    flyWheel.currentPower = flyWheel.defaultStartingPower;
+                    DPadLeft = true;
+                }
+            } else {
+                DPadLeft = false;
+            }
+
         }
 
-        if (DPadLeftPressed) {
-            if (!DPadLeft) {
-                flyWheel.currentlyRunning = true;
-                flyWheel.currentPower = flyWheel.defaultStartingPower;
-                DPadLeft = true;
-            }
-        } else {
-            DPadLeft = false;
+        if (testingTime.time() > 15) {
+            flyWheel.currentlyRunning = false;
         }
+        lastEncoder = flyWheel.FlyWheelMotor.getCurrentPosition();
+        lastRPM = (lastEncoder / 560) * 4;
+        telemetry.addData("FlyWheel", flyWheel.currentPower);
+        telemetry.addData("Time", String.valueOf(testingTime.time()));
+        telemetry.addData("RPM", lastRPM);
+        telemetry.addData("Enc", lastEncoder);
 
         flyWheel.currentPower = Range.clip(flyWheel.currentPower, 0, 1);
 
@@ -98,14 +153,6 @@ public class FlyWheelPID extends OpMode {
             flyWheel.FlyWheelMotor.setPower(flyWheel.currentPower);
         } else {
             flyWheel.FlyWheelMotor.setPower(0);
-        }
-
-        String RPM = String.valueOf((flyWheel.FlyWheelMotor.getCurrentPosition()/time)*60);
-
-        telemetry.addData("FlyWheel", String.valueOf(flyWheel.FlyWheelMotor.getPower()));
-        telemetry.addData("RPM", RPM);
-        if (Math.round(time) % 15 == 0) {
-            DbgLog.msg("RPM: " + RPM);
         }
 
     }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpCRIntake.java
@@ -0,0 +1,238 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.util.Range;
+
+import org.firstinspires.ftc.teamcode.robotlibrary.TBDName.DriveTrain;
+import org.firstinspires.ftc.teamcode.robotlibrary.TBDName.FlyWheel;
+import org.firstinspires.ftc.teamcode.robotlibrary.TBDName.Intake;
+import org.firstinspires.ftc.teamcode.robotlibrary.TBDName.Intake.IntakeSpec;
+import org.firstinspires.ftc.teamcode.robotlibrary.TBDName.Lift;
+import org.firstinspires.ftc.teamcode.robotlibrary.TBDName.TeleOpUtils;
+
+/**
+ * Created by Dynamic Signals on 10/21/2016.
+ */
+
+@com.qualcomm.robotcore.eventloop.opmode.TeleOp(name = "TeleOpCRIntake", group = "TeleOp")
+public class TeleOpCRIntake extends OpMode {
+
+    private boolean DPadUp = false;
+    private boolean DPadDown = false;
+    private boolean DPadRight = false;
+    private boolean DPadLeft = false;
+    private boolean DPadUp2 = false;
+    private boolean DPadDown2 = false;
+    private DriveTrain driveTrain;
+    private FlyWheel flyWheel;
+    private Intake intake;
+    private Lift lift;
+    private TeleOpUtils teleOpUtils;
+    CRServo intakeServo; // This object is temporary
+
+    @Override
+    public void init() {
+
+        driveTrain = new DriveTrain(hardwareMap);
+        flyWheel = new FlyWheel(hardwareMap);
+        intake = new Intake(hardwareMap);
+        lift = new Lift(hardwareMap);
+        teleOpUtils = new TeleOpUtils(gamepad1, gamepad2);
+        intakeServo = hardwareMap.crservo.get("IntakeServo");
+
+    }
+
+    @Override
+    public void loop() {
+
+        /*
+         * Controller 1 Controls --------------------------------------------------
+         */
+
+        /*
+         * Intake controls
+         * Left Trigger - Intake A sets power to -1
+         * Right Trigger - Intake A sets power to 1
+         * Left Bumper - Intake B sets power to -1
+         * Right Bumper - Intake B sets power to 1
+         */
+
+        if (gamepad1.left_trigger >= 0.5) {
+            intake.setIntakePower(IntakeSpec.A, -1);
+        }
+
+        if (gamepad1.right_trigger >= 0.5) {
+            intake.setIntakePower(IntakeSpec.A, 1);
+        }
+
+        if (gamepad1.left_trigger < 0.5 && gamepad1.right_trigger < 0.5) {
+            intake.stopIntake(IntakeSpec.A);
+        }
+
+        if (gamepad1.left_bumper) {
+            //intake.setIntakePower(IntakeSpec.B, -1);
+            intakeServo.setPower(1);
+        }
+
+        if (gamepad1.right_bumper) {
+            //intake.setIntakePower(IntakeSpec.B, 1);
+            intakeServo.setPower(-1);
+        }
+
+        if (!gamepad1.left_bumper && !gamepad1.right_bumper) {
+            //intake.stopIntake(IntakeSpec.B);
+            intakeServo.setPower(-0.1);
+        }
+
+        /*
+         * Fly wheel controls
+         * D-pad up - Increment the power by 0.05
+         * D-pad down - Decrease the power by 0.05
+         * D-pad right - Kill the motor
+         * D-pad left - Set the power to default no-matter the position
+         */
+
+        boolean DPadUpPressed = gamepad1.dpad_up;
+        boolean DPadDownPressed = gamepad1.dpad_down;
+        boolean DPadRightPressed = gamepad1.dpad_right;
+        boolean DPadLeftPressed = gamepad1.dpad_left;
+
+        if (DPadDownPressed) {
+            if (!DPadDown) {
+                flyWheel.currentlyRunning = true;
+                flyWheel.currentPower -= flyWheel.incrementValue;
+                DPadDown = true;
+            }
+        } else {
+            DPadDown = false;
+        }
+
+        if (DPadUpPressed) {
+            if (!DPadUp) {
+                flyWheel.currentlyRunning = true;
+                flyWheel.currentPower += flyWheel.incrementValue;
+                DPadUp = true;
+            }
+        } else {
+            DPadUp = false;
+        }
+
+        if (DPadRightPressed) {
+            if (!DPadRight) {
+                flyWheel.currentlyRunning = false;
+                DPadRight = true;
+            }
+        } else {
+            DPadRight = false;
+        }
+
+        if (DPadLeftPressed) {
+            if (!DPadLeft) {
+                flyWheel.currentlyRunning = true;
+                flyWheel.currentPower = flyWheel.defaultStartingPower;
+                DPadLeft = true;
+            }
+        } else {
+            DPadLeft = false;
+        }
+
+        flyWheel.currentPower = Range.clip(flyWheel.currentPower, 0, 1);
+
+        if (flyWheel.currentPower == 0) {
+            flyWheel.currentlyRunning = false;
+        }
+
+        if (flyWheel.currentlyRunning) {
+            flyWheel.FlyWheelMotor.setPower(flyWheel.currentPower);
+        } else {
+            flyWheel.FlyWheelMotor.setPower(0);
+        }
+
+        telemetry.addData("FlyWheel", String.valueOf(flyWheel.FlyWheelMotor.getPower()));
+
+
+        /*
+         * Driving controls
+         * Right Joystick - Arcade driving takes both x and y
+         */
+
+        // throttle: right_stick_y ranges from -1 to 1, where -1 is full up, and
+        // 1 is full down
+        // direction: right_stick_x ranges from -1 to 1, where -1 is full left
+        // and 1 is full right
+        float throttle = -gamepad1.right_stick_y;
+        float direction = gamepad1.right_stick_x;
+        float right = throttle - direction;
+        float left = throttle + direction;
+
+        // clip the right/left values so that the values never exceed +/- 1
+        right = Range.clip(right, -1, 1);
+        left = Range.clip(left, -1, 1);
+
+        // scale the joystick value to make it easier to control
+        // the robot more precisely at slower speeds.
+        right = (float) teleOpUtils.scaleInput(right);
+        left = (float) teleOpUtils.scaleInput(left);
+
+        // write the values to the motors
+        driveTrain.powerLeft(left);
+        driveTrain.powerRight(right);
+
+        /* Controller 1 telemetry data */
+        telemetry.addData("Drive power", "L: " + String.valueOf(left) + ", R: " + String.valueOf(right));
+
+        /*
+         * Controller 2 Controls --------------------------------------------------
+         */
+
+        /*
+         * Lift controls
+         * Right joystick Y Up - Lift positive power
+         * Right joystick Y Down - Lift negative power
+         * D-pad up (without the top bumper) - Put the lift servo up
+         * D-pad down (without the top bumper) - Put the lift servo down
+         * D-pad up (with top bumper) - Increment the servo up
+         * D-pad down (with top bumper) - Increment the servo down
+         */
+
+        double right_joystick_y = gamepad2.right_stick_y;
+        boolean liftManualMode = gamepad2.left_bumper;
+
+        lift.LiftMotor.setPower(teleOpUtils.scaleInput(right_joystick_y));
+        intake.setIntakePower(IntakeSpec.B, teleOpUtils.scaleInput(right_joystick_y)); // Kevin is using 2 motors for the lift
+
+        if (!liftManualMode) { // If it's automatic
+            if (gamepad2.dpad_up) {
+                lift.currentServoPosition = lift.maximum;
+            } if (gamepad2.dpad_down) {
+                lift.currentServoPosition = lift.minimum;
+            } if (gamepad2.dpad_left) {
+                lift.currentServoPosition = lift.ideal;
+            }
+        } else {
+            if (gamepad2.dpad_up) {
+                if (!DPadUp2) {
+                    lift.incrementServo();
+                    DPadUp2 = true;
+                }
+            } else {
+                DPadUp2 = false;
+            }
+            if (gamepad2.dpad_down) {
+                if (!DPadDown2) {
+                    lift.decrementServo();
+                    DPadDown2 = true;
+                }
+            } else {
+                DPadDown2 = false;
+            }
+        }
+        lift.currentServoPosition = Range.clip(lift.currentServoPosition, -1, 1);
+        lift.LiftServo.setPosition(lift.currentServoPosition);
+
+
+        telemetry.addData("Lift Servo", String.valueOf(lift.currentServoPosition));
+
+    }
+}
