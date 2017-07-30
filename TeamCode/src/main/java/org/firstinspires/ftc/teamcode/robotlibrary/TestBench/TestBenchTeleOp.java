74a23c9f6fe73d2294021a14a3fd3f4cb25b321a
Leo Forney <forneyleo@gmail.com>
2017-07-30 10:58:46 -0500

    Complete teleop for testbench and demo robots

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotlibrary/TestBench/TestBenchTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotlibrary/TestBench/TestBenchTeleOp.java
@@ -18,69 +18,106 @@ public class TestBenchTeleOp extends OpMode {
 
     DriveTrain driveTrain;
     TeleOpUtils teleOpUtils;
-    boolean twoMotors = true;
+    boolean twoMotors = false;
+    boolean tankDrive = false;
     DcMotor LeftMotor;
     DcMotor RightMotor;
+    int selectionStage = 0;
 
     boolean RightDPad = false;
+
 
     @Override
     public void init() {
 
-        if (!twoMotors) {
-            driveTrain = new DriveTrain(hardwareMap);
-        } else {
-            LeftMotor = hardwareMap.dcMotor.get("LeftMotor");
-            RightMotor = hardwareMap.dcMotor.get("RightMotor");
-            RightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
-        }
-
         teleOpUtils = new TeleOpUtils(gamepad1, gamepad2);
-
 
     }
 
     @Override
     public void init_loop() {
-        if (gamepad1.dpad_right) {
-            if (!RightDPad) {
-                if (twoMotors) {
-                    twoMotors = false;
-                } else {
-                    twoMotors = true;
+        if (selectionStage == 0) {
+            if (gamepad1.dpad_right) {
+                if (!RightDPad) {
+                    if (twoMotors) {
+                        twoMotors = false;
+                    } else {
+                        twoMotors = true;
+                    }
+                    RightDPad = true;
                 }
-                RightDPad = true;
+            } else {
+                RightDPad = false;
             }
-        } else {
-            RightDPad = false;
+            if (gamepad1.dpad_up) {
+                selectionStage++;
+            }
+
+            telemetry.addData("Motor mode", (twoMotors ? "Two Motors": "Four Motors"));
+            telemetry.addData("", "Change motor mode by right dpad");
+            telemetry.addData(".", "Dpad up to go to next stage");
         }
-        telemetry.addData("Motor mode", (twoMotors ? "Two Motors": "Four Motors"));
-        telemetry.addData("", "Change motor mode by right dpad");
+        if (selectionStage == 1) {
+            if (gamepad1.dpad_right) {
+                if (!RightDPad) {
+                    if (tankDrive) {
+                        tankDrive = false;
+                    } else {
+                        tankDrive = true;
+                    }
+                    RightDPad = true;
+                }
+            } else {
+                RightDPad = false;
+            }
+            telemetry.addData("Drive mode", (tankDrive ? "Tank": "Arcade"));
+            telemetry.addData("", "Change drive mode by right dpad");
+        }
+    }
+
+    @Override
+    public void start() {
+        if (!twoMotors) {
+            driveTrain = new DriveTrain(hardwareMap);
+        } else {
+            LeftMotor = hardwareMap.dcMotor.get("leftMotor");
+            RightMotor = hardwareMap.dcMotor.get("rightMotor");
+            RightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        }
     }
 
     @Override
     public void loop() {
 
-        // throttle: right_stick_y ranges from -1 to 1, where -1 is full up, and
-        // 1 is full down
-        // direction: right_stick_x ranges from -1 to 1, where -1 is full left
-        // and 1 is full right
-        float throttle = -gamepad1.right_stick_y;
-        float direction = gamepad1.right_stick_x;
-        if (twoMotors) {
-            direction = -direction;
+
+        float left = 0;
+        float right = 0;
+        if (!tankDrive) {
+            // throttle: right_stick_y ranges from -1 to 1, where -1 is full up, and
+            // 1 is full down
+            // direction: right_stick_x ranges from -1 to 1, where -1 is full left
+            // and 1 is full right
+            float throttle = -gamepad1.right_stick_y;
+            float direction = gamepad1.right_stick_x;
+            if (twoMotors) {
+                direction = -direction;
+            }
+            right = throttle - direction;
+            left = throttle + direction;
+
+            // clip the right/left values so that the values never exceed +/- 1
+            right = Range.clip(right, -1, 1);
+            left = Range.clip(left, -1, 1);
+
+            // scale the joystick value to make it easier to control
+            // the robot more precisely at slower speeds.
+            right = (float) teleOpUtils.scaleInput(right);
+            left = (float) teleOpUtils.scaleInput(left);
+        } else {
+            left = (float) teleOpUtils.scaleInput(-gamepad1.left_stick_y);
+            right = (float) teleOpUtils.scaleInput(-gamepad1.right_stick_y);
         }
-        float right = throttle - direction;
-        float left = throttle + direction;
 
-        // clip the right/left values so that the values never exceed +/- 1
-        right = Range.clip(right, -1, 1);
-        left = Range.clip(left, -1, 1);
-
-        // scale the joystick value to make it easier to control
-        // the robot more precisely at slower speeds.
-        right = (float) teleOpUtils.scaleInput(right);
-        left = (float) teleOpUtils.scaleInput(left);
 
         // write the values to the motors
         if (!twoMotors) {
